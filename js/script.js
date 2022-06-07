91312d3409a79f3325ef8ecffe18db5c9dffdde9
adilxan1998 <odilxon.esonaliyev1998@gmail.com>
2022-06-07 13:45:08 +0500

    undifind to`g`irlandi

--- js/script.js
+++ js/script.js
@@ -8,14 +8,16 @@ const contacts = []
 
 elForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  elResult.innerHTML = "";
+  
   const inputNameValue = elInputName.value;
   const inputRelativeValue = elInputRelative.value;
-  const inputNumbervalue = elInputNumber.value;
+  const inputNumberValue = elInputNumber.value;
 
   const contact = {
     name : inputNameValue,
     relative : inputRelativeValue,
-    number : inputNumbervalue,
+    number : inputNumberValue,
   }
 
     contacts.push(contact)
@@ -23,20 +25,16 @@ elForm.addEventListener("submit", function (e) {
     elInputName.value = null;
     elInputRelative.value = null;
     elInputNumber.value = null;
-
     
-    for ( cont of contacts){
-
+    for (let cont of contacts){
       let nameP = document.createElement("p")
       let relP = document.createElement("p")
       let numP = document.createElement("p")
 
       nameP.textContent = `${cont.name}`
-      relP.textContent = `${cont.relaship}`
+      relP.textContent = `${cont.relative}`
       numP.textContent = `${cont.number}`
 
-      elResult.appendChild(nameP)
-      elResult.appendChild(relP)
-      elResult.appendChild(numP)
+      elResult.append(nameP, relP, numP)
     }
 })
